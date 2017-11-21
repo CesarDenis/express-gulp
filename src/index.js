3c689a815b7ba7966157bb5c7971913cde364e53
Cesar Denis <cesar@cesardenis.com>
2017-11-21 16:44:15 -0200

    Add default HTML template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-const express = require('express');
 const winston = require('winston');
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'It works!' });
-});
+const app = require('./app');
 
 app.listen(3000, () => {
   winston.info('Express: 3000');
